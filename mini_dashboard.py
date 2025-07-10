9beaac785d9cadd7863404b8f7a205edaa2dde13
Daniel Córdoba <157246808+danielcba@users.noreply.github.com>
2025-07-10 15:27:39 -0300

    Update mini_dashboard.py

--- mini_dashboard.py
+++ mini_dashboard.py
@@ -44,7 +44,7 @@ METRICAS_CLAVE = {
 }
 
 # Lista de tickers disponibles (acciones locales e internacionales)
-TICKERS = ['ES', 'IBM', 'ALUA.BA', 'BMA.BA','CGPA2.BA', 'EDN.BA', 'GGAL.BA',
+TICKERS = ['ALUA.BA', 'BYMA.BA', 'BMA.BA','CGPA2.BA', 'EDN.BA', 'GGAL.BA',
            'METR.BA', 'MSFT', 'PAMP.BA', 'SUPV.BA', 'TGSU2.BA', 'YPFD.BA']
 
 # Sección lateral de configuración del usuario
