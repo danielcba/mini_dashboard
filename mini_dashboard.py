69f373eed0aeb299fe3acd42d7f510ef21fb143e
Daniel Córdoba <157246808+danielcba@users.noreply.github.com>
2025-08-15 13:29:57 -0300

    Update mini_dashboard.py

--- mini_dashboard.py
+++ mini_dashboard.py
@@ -52,7 +52,7 @@ METRICAS_CLAVE = {
 }
 
 # Lista de tickers disponibles (acciones locales e internacionales)
-TICKERS = ['MELI.BA', 'AOS', 'AAPL', 'AMZN', 'AVGO', '^GSPC', 'GOOG', 'GOOGL', 'IBM', 'JNJ', 'KO', 'META', 'MSFT', 'NVDA', 'PM', 'SWK', 'TSLA', 'UNH']
+TICKERS = ['AOS', 'AAPL', 'AMZN', 'AVGO', '^GSPC', 'GOOG', 'GOOGL', 'IBM', 'JNJ', 'KO', 'MELI', 'META', 'MSFT', 'NVDA', 'PM', 'SWK', 'TSLA', 'UNH']
 
 # Sección lateral de configuración del usuario
 st.sidebar.title("⚙️ Configuración")
